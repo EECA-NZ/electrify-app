edd744550294f73c2ed250d2db75de7b617f9cfe
willcattoneeca <will.catton@eeca.govt.nz>
2024-10-07 17:38:37 +1300

    Fix pylint

--- app/api/component_savings_endpoints.py
+++ app/api/component_savings_endpoints.py
@@ -1,6 +1,17 @@
-from fastapi import FastAPI
+"""
+This module provides functions to estimate savings for individual
+components of the home.
+"""
+
 import logging
-from ..models.answers import HeatingAnswers, HotWaterAnswers, CooktopAnswers, DrivingAnswers, SolarAnswers, YourHomeAnswers
+from fastapi import FastAPI
+from ..models.answers import (
+    HeatingAnswers,
+    HotWaterAnswers,
+    CooktopAnswers,
+    DrivingAnswers,
+    YourHomeAnswers,
+)
 from ..services.cost_calculator import calculate_savings_options
 
 # Set up logging
@@ -8,33 +19,40 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
+# pylint: disable=broad-exception-caught
 @app.post("/heating/savings")
 async def heating_savings(heating_answers: HeatingAnswers, your_home: YourHomeAnswers):
     """
     Endpoint to calculate savings for heating options.
     """
     try:
-        logger.info(f"Received heating answers: {heating_answers}")
-        options = calculate_savings_options(heating_answers, 'main_heating_source', your_home)
-        logger.info(f"Found heating options: {options}")
+        logger.info("Received heating answers: %s", heating_answers)
+        options = calculate_savings_options(
+            heating_answers, "main_heating_source", your_home
+        )
+        logger.info("Found heating options: %s", options)
         return {"options": options}
     except Exception as e:
-        logger.error(f"Error calculating heating savings: {e}")
+        logger.error("Error calculating heating savings: %s", e)
         return {"error": "Error calculating heating savings"}
 
 
 @app.post("/hot_water/savings")
-async def hot_water_savings(hot_water_answers: HotWaterAnswers, your_home: YourHomeAnswers):
+async def hot_water_savings(
+    hot_water_answers: HotWaterAnswers, your_home: YourHomeAnswers
+):
     """
     Endpoint to calculate savings for hot water heating options.
     """
     try:
-        logger.info(f"Received hot water answers: {hot_water_answers}")
-        options = calculate_savings_options(hot_water_answers, 'hot_water_heating_source', your_home)
-        logger.info(f"Found hot water options: {options}")
+        logger.info("Received hot water answers: %s", hot_water_answers)
+        options = calculate_savings_options(
+            hot_water_answers, "hot_water_heating_source", your_home
+        )
+        logger.info("Found hot water options: %s", options)
         return {"options": options}
     except Exception as e:
-        logger.error(f"Error calculating hot water savings: {e}")
+        logger.error("Error calculating hot water savings: %s", e)
         return {"error": "Error calculating hot water savings"}
 
 
@@ -44,12 +62,12 @@ async def cooktop_savings(cooktop_answers: CooktopAnswers, your_home: YourHomeAn
     Endpoint to calculate savings for cooking options.
     """
     try:
-        logger.info(f"Received cooktop answers: {cooktop_answers}")
-        options = calculate_savings_options(cooktop_answers, 'cooktop', your_home)
-        logger.info(f"Found cooktop options: {options}")
+        logger.info("Received cooktop answers: %s", cooktop_answers)
+        options = calculate_savings_options(cooktop_answers, "cooktop", your_home)
+        logger.info("Found cooktop options: %s", options)
         return {"options": options}
     except Exception as e:
-        logger.error(f"Error calculating cooktop savings: {e}")
+        logger.error("Error calculating cooktop savings: %s", e)
         return {"error": "Error calculating cooktop savings"}
 
 
@@ -59,10 +77,10 @@ async def driving_savings(driving_answers: DrivingAnswers, your_home: YourHomeAn
     Endpoint to calculate savings for driving options.
     """
     try:
-        logger.info(f"Received driving answers: {driving_answers}")
-        options = calculate_savings_options(driving_answers, 'vehicle_type', your_home)
-        logger.info(f"Found driving options: {options}")
+        logger.info("Received driving answers: %s", driving_answers)
+        options = calculate_savings_options(driving_answers, "vehicle_type", your_home)
+        logger.info("Found driving options: %s", options)
         return {"options": options}
     except Exception as e:
-        logger.error(f"Error calculating driving savings: {e}")
+        logger.error("Error calculating driving savings: %s", e)
         return {"error": "Error calculating driving savings"}
--- app/services/helpers.py
+++ app/services/helpers.py
@@ -2,8 +2,8 @@
 Module for generic helper functions.
 """
 
-def answer_options(object, field):
+def answer_options(my_object, field):
     """
     For a given field on a pydantic model, return the possible answer options.
     """
-    return type(object).model_fields[field].annotation.__args__
+    return type(my_object).model_fields[field].annotation.__args__
